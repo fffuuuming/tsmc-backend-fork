8e4d1af7e8a97f470d45c96de22c425474d4075d
ongdisheng <63136897+ongdisheng@users.noreply.github.com>
2025-05-06 21:54:11 +0800

    feat: Added Prometheus setup (#20)

--- app/main.py
+++ app/main.py
@@ -3,18 +3,20 @@ import os
 from dotenv import load_dotenv
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from prometheus_fastapi_instrumentator import Instrumentator
 
 from app.routers import earthquake
 
 # load env variables
 load_dotenv()
+
 app = FastAPI()
+Instrumentator().instrument(app).expose(app)
 
 # enable CORS related config
 origins = [
     os.environ.get("FRONTEND_BASE_URL"),
 ]
-
 app.add_middleware(
     CORSMiddleware,
     allow_origins=origins,
@@ -22,6 +24,5 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
-
 
 app.include_router(earthquake.router, prefix="/api", tags=["earthquake"])
